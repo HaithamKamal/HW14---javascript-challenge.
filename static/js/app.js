53013f96a7c3395552cb34feef2eec3f7dd46b1b
Haitham K Mostafa <64391312+HaithamKamal@users.noreply.github.com>
2020-09-27 00:40:11 -0500

    Add files via upload

---
+++ static/js/app.js
@@ -0,0 +1,68 @@
+// from data.js
+var filteredData = data;
+let tableMatch = null;
+
+// set the reference to the table body and initialize filter flag
+let tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+let filtered = 0;
+
+// fill in full table by default
+filteredData.forEach(row => {
+  tbody.append("tr");
+
+  for (key in row){
+      const cell = tbody.append("td");
+      cell.text(row[key]);
+  }
+});
+
+button.on("click", function() {
+// from data.js
+var filteredData = data;
+// Select the input element and get the raw HTML node
+var inputElement = d3.select("#datetime");
+
+// Get the value property of the input element
+var inputValue = inputElement.property("value");
+var inputValueCity = d3.select("#city").property("value");
+var inputValueState = d3.select("#state").property("value");
+var inputValueCountry = d3.select("#country").property("value");
+var inputValueShape = d3.select("#shape").property("value");
+
+if (inputValue != ""){
+  filteredData = filteredData.filter(UFOsighting=> UFOsighting.datetime === inputValue); 
+}
+if (inputValueCity!=""){
+  filteredData = filteredData.filter(UFOsighting=> UFOsighting.city === inputValueCity); 
+
+}
+if (inputValueState != ""){
+  filteredData = filteredData.filter(UFOsighting=> UFOsighting.state === inputValueState); 
+}
+if (inputValueCountry != ""){
+  filteredData = filteredData.filter(UFOsighting=> UFOsighting.country === inputValueCountry); 
+}
+
+if (inputValueShape != ""){
+  filteredData = filteredData.filter(UFOsighting=> UFOsighting.shape === inputValueShape); 
+}
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+tbody.html("")
+
+// Use d3 to update each cell's text with
+
+filteredData.forEach(function(weatherReport) {
+  console.log(weatherReport);
+  var row = tbody.append("tr");
+  Object.entries(weatherReport).forEach(function([key, value]) {
+    console.log(key, value);
+    // Append a cell to the row for each value
+    // in the weather report object
+    var cell = row.append("td");
+    cell.text(value);
+  });
+})
+})
+
